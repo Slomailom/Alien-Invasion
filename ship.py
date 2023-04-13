8ce267af89cc79a4203cdf0658d1ba967de34b60
Slomailom <sghigh@rambler.ru>
2023-04-13 22:13:13 +0200

    speed and screen borders

--- ship.py
+++ ship.py
@@ -6,22 +6,28 @@ class Ship():
         """Инициализируем корабль и задаём его начальную позицию"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Загружаем изображение корабля
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         # Каждый новый корабль появляется у нижнего края экрана
         self.rect.midbottom = self.screen_rect.midbottom
+        # Сохранение вещественной координаты центра корабля
+        self.x = float(self.rect.x)
         # Флаг пермещения
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Обновляет позицию корабля с учётом флагов"""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        # Обновляется атрибут x, не rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        # Обновление атрибута rect на основании self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Рисует корабль в текущей позиции"""
